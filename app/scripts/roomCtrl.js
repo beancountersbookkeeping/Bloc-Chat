a9e7a5e3602cd29957bde78bec103c033e0bd1e1
beancountersbookkeeping <kay@thebeancountersbookkeeping.com>
2017-03-12 19:25:51 -0400

    3/12 working

--- app/scripts/roomCtrl.js
+++ app/scripts/roomCtrl.js
@@ -1,8 +1,9 @@
 (function(){
     function roomCtrl($scope, Room, $state, Message){
-        // $scope.rooms = Room.all;
+        $scope.rooms = Room.all;
         $scope.test = "Room Show Page!";
         $scope.roomId = $state.params.roomId;
+        $scope.name = $state.params.name;
 
         $scope.messages = Message.getByRoomID($scope.roomId);
     }
